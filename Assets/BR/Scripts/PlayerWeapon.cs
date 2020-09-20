19feea1c29e8e25549272c7f65974d0df30a8276
CH <CH>
2020-09-20 11:35:51 -0700

    Initial commit

---
+++ Assets/BR/Scripts/PlayerWeapon.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PlayerWeapon : MonoBehaviour
+{
+    [Header("Stats")]
+    public int damage;
+    public int curAmmo;
+    public int maxAmmo;
+    public float bulletSpeed;
+    public float shootRate;
+
+    private float lastShootTime;
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawnPos;
+
+    private PlayerController player;
+
+
+    void Awake()
+    {
+        //get required components
+        player = GetComponent<PlayerController>();
+    }
+
+    public void TryShoot()
+    {
+        //Can we shoot?
+        if (curAmmo <= 0 || Time.time - lastShootTime < shootRate)
+        {//Cannot shoot
+            return;
+        }
+
+        curAmmo--;
+        lastShootTime = Time.time;
+
+        //Update ammo ui
+        GameUI.instance.UpdateAmmoText();
+
+
+        // spawn the bullet
+        player.photonView.RPC("SpawnBullet", RpcTarget.All, bulletSpawnPos.transform.position, Camera.main.transform.forward);
+    }
+
+    [PunRPC]
+    void SpawnBullet(Vector3 pos, Vector3 dir)
+    {
+        // spawn and orient it
+        GameObject bulletObj = Instantiate(bulletPrefab, pos, Quaternion.identity);
+        bulletObj.transform.forward = dir;
+
+        Debug.Log("Spawned with dir " + dir * bulletSpeed);
+
+        // get bullet script
+        Bullet bulletScript = bulletObj.GetComponent<Bullet>();
+
+        // initialize it and set the velocity
+        bulletScript.Initialize(damage, player.id, player.photonView.IsMine);
+        //bulletScript.rb.velocity = dir * bulletSpeed;
+        bulletScript.rb.AddForce(dir * bulletSpeed, ForceMode.VelocityChange);
+    }
+
+
+
+    #region Pickups
+
+    [PunRPC]
+    public void GiveAmmo(int ammoToGive)
+    {
+        curAmmo = Mathf.Clamp(curAmmo + ammoToGive, 0, maxAmmo);
+
+        //Update UI
+        GameUI.instance.UpdateAmmoText();
+    }
+
+    #endregion Pickups
+
+
+
+}
